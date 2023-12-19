79b1a533a38c05961e5b70e10f27ed1194d9e008
andreypotkas <potkasa@gmail.com>
2023-12-19 19:14:36 +0300

    refactor main.ts: change cors

--- src/main.ts
+++ src/main.ts
@@ -16,9 +16,7 @@ const origins = [
 ];
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: { origin: origins },
-  });
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: origins,
